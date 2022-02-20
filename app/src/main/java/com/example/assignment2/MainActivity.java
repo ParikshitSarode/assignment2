81f370620186e9b28a719c9ea6111a4f5ccaf98d
parikshit21063 <parikshit21063@iiitd.ac.in>
2022-02-20 14:25:52 +0530

    second commit

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -6,13 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
     LinearLayout Nav;
-    Button startser,stopser;
+    TextView counter;
+    Button startser,stopser,prev,next;
+    int countervalue = 1, total = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +24,19 @@ public class MainActivity extends AppCompatActivity {
         Nav = (LinearLayout) findViewById(R.id.nav);
         startser = (Button) findViewById(R.id.button);
         stopser = (Button) findViewById(R.id.button2);
+        counter = (TextView) findViewById(R.id.textView4);
+        prev =  (Button) findViewById(R.id.button3);
+        next  =  (Button) findViewById(R.id.button4);
         Nav.setVisibility(View.INVISIBLE);
 
         startser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (Nav.getVisibility()==View.INVISIBLE)
+                Toast.makeText(MainActivity.this, "Service Started", Toast.LENGTH_SHORT).show();
+                else if (Nav.getVisibility()==View.VISIBLE)
+                    Toast.makeText(MainActivity.this, "Service already running", Toast.LENGTH_SHORT).show();
                 Nav.setVisibility(View.VISIBLE);
-
                 //start service to get news
             }
         });
@@ -36,6 +45,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //stop service
+                Toast.makeText(MainActivity.this, "Service Stopped", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //goto prev news and update counter view
+                if(countervalue>1){
+                    countervalue--;
+                    counter.setText(countervalue+"/"+total);
+                }
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(countervalue<total){
+                    countervalue++;
+                    counter.setText(countervalue+"/"+total);
+                }
             }
         });
 
